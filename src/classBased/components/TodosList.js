3b3d5280bbd6b3aaaa8052eee04239ad52001e1c
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-06-04 12:51:42 +0200

    Working with event handling

--- src/classBased/components/TodosList.js
+++ src/classBased/components/TodosList.js
@@ -1,12 +1,19 @@
 /*eslint-disable*/
 import React from 'react';
-import TodoItem from "./TodoItem";
+import TodoItem from './TodoItem';
+
 class TodosList extends React.Component {
   render() {
     return (
       <ul>
         {this.props.todos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} />
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleChangeProps={this.props.handleChangeProps}
+            deleteTodoProps={this.props.deleteTodoProps}
+            setUpdate={this.props.setUpdate}
+          />
         ))}
       </ul>
     );
